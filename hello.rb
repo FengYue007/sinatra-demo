9cf713b697a8a9a4630abad40acd87d32c451d2e
FengYue <10761101@gm.nfu.edu.tw>
2021-07-07 17:56:53 +0800

    新增hello2.rb 並 修改README.md 新增帶參數的方法

--- hello.rb
+++ hello.rb
@@ -2,6 +2,7 @@ require "sinatra" # 引用 sinatra 模組
 
 get "/" do
   "Home!"
+  # "你好，世界，現在是 #{Time.now}"
 end
 
 get "/hello" do
---
+++ hello2.rb
@@ -0,0 +1,14 @@
+# sinatra程式，帶點資料給它。
+require 'sinatra'
+
+get '/' do
+  "歡迎來到貓的世界，有貓真好！"
+end
+
+get '/cats' do
+  "黑貓白貓，會呼嚕的貓就是好貓！"
+end
+
+get '/cats/:id' do
+  "你好，你是第 #{params[:id]} 號的貓!"
+end
